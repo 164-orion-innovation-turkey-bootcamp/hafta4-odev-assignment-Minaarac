60b3616d89e1f9742a6e6e69c5c173410ddd2387
Minaarac <minaarac@gmail.com>
2022-03-17 20:43:18 +0300

    Single Product Page

--- minaShop/src/app/app.module.ts
+++ minaShop/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { ProductsingleComponent } from './productsingle/productsingle.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    ProductsingleComponent
   ],
   imports: [
     BrowserModule,
