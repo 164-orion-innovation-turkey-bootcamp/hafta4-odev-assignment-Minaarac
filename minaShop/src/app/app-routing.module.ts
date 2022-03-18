d1ee130c300c86347779f8247afdad52f4facd0d
Minaarac <minaarac@gmail.com>
2022-03-18 21:21:07 +0300

    Login-signup

--- minaShop/src/app/app-routing.module.ts
+++ minaShop/src/app/app-routing.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsingleComponent } from './productsingle/productsingle.component';
 import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ShopComponent } from './shop/shop.component';
+import { OrdersComponent } from './orders/orders.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   { path:"", component:HomeComponent },
-  { path:"product-single", component:ProductsingleComponent }
+  { path:"product-single", component:ProductsingleComponent },
+  { path:"cart", component:CartComponent },
+  { path:"checkout", component:CheckoutComponent },
+  { path:"shop", component:ShopComponent },
+  { path:"order", component:OrdersComponent },
+  { path:"login", component:LoginComponent },
+  { path:"signup", component:SignupComponent },
 ];
 
 @NgModule({
--- minaShop/src/app/app.module.ts
+++ minaShop/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -8,6 +9,13 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
 import { ProductsingleComponent } from './productsingle/productsingle.component';
+import { CartComponent } from './cart/cart.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ShopComponent } from './shop/shop.component';
+import { OrdersComponent } from './orders/orders.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
@@ -15,12 +23,21 @@ import { ProductsingleComponent } from './productsingle/productsingle.component'
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    ProductsingleComponent
+    ProductsingleComponent,
+    CartComponent,
+    CheckoutComponent,
+    ShopComponent,
+    OrdersComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SlickCarouselModule
+    SlickCarouselModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
